0e0d92a65ca0c835586f2a3de1297514fb2a35d9
Mathieu Landry <mthlandry@gmail.com>
2025-09-09 20:24:29 -0400

    added function to update recipe and machine choices

---
+++ function_change.R
@@ -0,0 +1,24 @@
+change_recipe <- function(data, product, recipe) {
+  input <- data
+  
+  output <- input %>%
+    mutate(preferred = case_when(
+      product_name == product & name == recipe ~ 1,
+      product_name == product ~ 0,
+      TRUE ~ preferred
+    ))
+  
+  return(output)
+}
+
+change_machine <- function(data, machine, value = 1) {
+  input <- data
+  
+  output <- input %>%
+    mutate(accessible = case_when(
+      machine_name == machine ~ value,
+      TRUE ~ accessible
+    ))
+  
+  return(output)
+}
