f9e5e76c1221fe3af094b5446f2ebe8dcf4d847b
Mathieu <mthlandry@gmail.com>
2025-12-08 22:05:23 -0500

    started implementation of a planning step

--- draft.R
+++ draft.R
@@ -17,18 +17,20 @@ plan_project <- function(data_input, data_recipes, data_machines, product, quant
   data_recipes <- data_recipes %>%
     filter(preferred == 1)
   
-  data_all <- data_input %>%
-    right_join(data_recipes, by = c("product_name","name")) %>%
+  data_pre_curated <- data_input %>%
+    right_join(data_recipes, by = c("product_name","name")) 
+  
+  data_curated <- data_pre_curated %>%
     distinct(name, product_name, ing_name, .keep_all = TRUE)
   
-  level_max <- data_all %>%
+  level_max <- data_curated %>%
     filter(product_name == product) %>%
     select(product_name, recipe_level) %>%
     distinct(product_name, recipe_level) %>%
     pull(recipe_level)
   
   #start by iterating downward and making a to do list
-  to_do <- recursive_task_table(data_all, product, quantity) %>%
+  to_do <- recursive_task_table(data_curated, product, quantity) %>%
     mutate(
       ingredient_hierarchy = paste0(strrep("-", 4 * recurse_level), ingredient_name)
     )
@@ -39,20 +41,26 @@ plan_project <- function(data_input, data_recipes, data_machines, product, quant
         .groups = "drop") %>%
     arrange(desc(level))
   
+  plan_simple <- to_do_simple %>%
+    mutate(desc_col = n():1) %>%
+    left_join(data_recipes, by = c("ingredient_name" = "product_name")) %>%
+    select(ingredient_name, level, quantity_needed, desc_col, name) %>%
+    left_join(data_pre_curated, by = c("name")) %>%
+    select(ingredient_name, level, quantity_needed, desc_col, name, recipe_no, product_no, product_name, product_amount, recipe_time, machine_name, crafting_speed, production_per_machine) %>%
+    distinct()
   
-  return(list(to_do,to_do_simple))
-  # to_do_list <- data.frame()
-  # 
-  # interested <- c(product)
-  # needed <- c(quantity)
-  # 
+  return(list(to_do, to_do_simple, plan_simple))
 
+  #fix choice_recipes for iron then confirm each product only has one recipe
+  #iterate on recipes in plan_simple then choose with select_machines()
+  #output no of machines doing what
+  #output an in/out table then update the main bus table
+  #implement the bus option
 }
 
 #test the function
-test <- plan_project(recipes, choices_recipes, choices_machines, "bulk-inserter", 0.5, usebus="Y")
+test <- plan_project(recipes, choices_recipes, choices_machines, "bulk-inserter", 10, usebus="Y")
 test1 <- test[[1]]
 test2 <- test[[2]]
-#then go upward and get info from big table
-#output an in/out table then update the main bus table
+test3 <- test[[3]]
 #create an excel table of level recipes to add to big table (uranium mining may be an issue)
